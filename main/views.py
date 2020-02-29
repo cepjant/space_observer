7ec6ce28c521bcfc5b530aff5e3e163c16632566
cepjant <vernigor191919@gmail.com>
2020-02-29 13:58:00 +1000

    initial commit

--- main/views.py
+++ main/views.py
@@ -1,25 +1,22 @@
+import json
+import folium
+import urllib.request
+
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import HttpResponse
-import urllib.request
-import json
 from datetime import datetime
-import folium
 
 
 class Main(View):
     def get(self, request):
-        ISS1 = ISS()
-        map1 = Map()
-        map1.create_map()
-        crew = Crew()
-        people_count, names_crafts, crew_message = crew.get_crew()
-        date, time, ISS_message, position = ISS1.get_info()
+        Map.create_map()
+        people_count, names_crafts, crew_message = Crew.get_crew()
+        date, time, iss_message, position = ISS.get_info()
         context = {
             'time': time,
             'date': date,
             'position': position,
-            'ISS_message': ISS_message,
+            'ISS_message': iss_message,
             'people_count': people_count,
             'names_crafts': names_crafts,
             'crew_message': crew_message
@@ -28,7 +25,8 @@ class Main(View):
 
 
 class ISS:
-    def get_info(self):
+    @staticmethod
+    def get_info():
         response = urllib.request.urlopen("http://api.open-notify.org/iss-now.json")
         obj = json.loads(response.read())
         date_time = (datetime.utcfromtimestamp(obj['timestamp']).strftime('%Y-%m-%d %H:%M:%S'))
@@ -44,7 +42,8 @@ class ISS:
 
 
 class Crew:
-    def get_crew(self):
+    @staticmethod
+    def get_crew():
         response = urllib.request.urlopen("http://api.open-notify.org/astros.json")
         obj = json.loads(response.read())
         people_count = obj['number']
@@ -63,12 +62,12 @@ class Crew:
 
 
 class Map:
-    def create_map(self):
-        ISS1 = ISS()
-        lat, long = ISS1.get_info()[3]['latitude'], ISS1.get_info()[3]['longitude']  # получение широты и долготы МКС
-        map = folium.Map(location=[37.143977, -121.963908], zoom_start=1,
+    @staticmethod
+    def create_map():
+        lat, long = ISS.get_info()[3]['latitude'], ISS.get_info()[3]['longitude']  # получение широты и долготы МКС
+        main_map = folium.Map(location=[37.143977, -121.963908], zoom_start=1,
                          tiles='CartoDB dark_matter')  # установка карты
         folium.CircleMarker(location=(lat, long), radius=9, popup='ISS',
                             fill_color='white', color="gray", fill_opacity=0.9).add_to(map)  # установка маркера МКС
 
-        map.save('templates/map.html')  # сохранение карты в 'templates' проекта
+        main_map.save('templates/map.html')  # сохранение карты в 'templates' проекта
